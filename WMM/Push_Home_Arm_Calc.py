f6f978495674e92b6092d6444d6849562ec6b6c5
clstlaurent <clstlaurent@folivoraapps.com>
2018-04-21 22:23:59 -0400

    mapping fix

--- WMM/Push_Home_Arm_Calc.py
+++ WMM/Push_Home_Arm_Calc.py
@@ -99,7 +99,7 @@ def home2Handle_Control(t1_f,t2_f):
     while (e1>=1):
 
         #tX = measured angle
-        tX_new = n.interp(joint1_queue.pop(),[35,275],[120,-120])
+        tX_new = n.interp(joint1_queue.pop(),[35,275],[-120,120])
         
         print("Popped from t1 bitches: " + str(tX_new))
         
@@ -140,7 +140,7 @@ def home2Handle_Control(t1_f,t2_f):
     while (e2>=1):
 
         #tX = measured angle
-        tX_new = n.interp(joint2_queue.pop(),[59,299],[-120,120])
+        tX_new = n.interp(joint2_queue.pop(),[59,299],[120,-120])
         print("Joint 2: " + str(tX_new))
 
         if((t2_f-tX_new)<=1):
